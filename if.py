fe579e9e7d8e978d7ecc019eafc1c6e901df7c12
Johann S Cardenas <jscardenase@unal.edu.co>
2024-04-23 15:47:53 -0500

    if else statements

---
+++ if.py
@@ -0,0 +1,27 @@
+#Conditionals
+if True:
+    print(True)
+else:
+    print(False)
+
+if "text" == "other word":
+    print(True)
+else:
+    print(False)
+
+if 1 == 1:
+    print(True)
+else:
+    print(False)
+
+if 1 > 100:
+    print(True)
+else:
+    print(False)
+
+#Identation example: press the key Tab to make the ident
+if "text" == "text":
+    print(True)
+    print("inside of the identation range")
+
+print("outside of the identation range")
